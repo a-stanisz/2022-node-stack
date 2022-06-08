e74e903598ecd77057225ac236a5bf246a92bed2
a-stanisz <hello@adamstaniszewski.com>
2022-06-08 12:18:56 +0200

    Add Express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('hello');
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+const port = Number(process.env.PORT ?? 8080);
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.json ({hello: 'world'});
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server started at port ${port}`);
+});
