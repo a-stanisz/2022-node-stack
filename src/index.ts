957b4582b74d71fd9051ec4e1db95da1acf8e2c4
a-stanisz <hello@adamstaniszewski.com>
2022-06-08 16:00:10 +0200

    Try to seed db

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,55 @@
+import { PrismaClient } from '@prisma/client';
+// import { randomUUID } from 'crypto';
 import express from 'express';
 import morgan from 'morgan';
+
+const db = new PrismaClient({ log: ['error', 'info', 'query', 'warn'] });
+
+// const genId = () => randomUUID();
+
+// const seedDB = async () => {
+//   if ((await db.user.count()) === 0) {
+//     await db.user.createMany({
+//       data: [
+//         {
+//           email: 'email@example.com',
+//           name: 'Admin',
+//           role: 'ADMIN',
+//         },
+//         {
+//           email: 'john.doe@example.com',
+//           name: 'John Doe',
+//         },
+//       ],
+//     });
+//   }
+//   if ((await db.post.count()) === 0) {
+//     await db.post.createMany({
+//       data: [
+//         {
+//           id: genId(),
+//           slug: 'node-stack',
+//           title: '2022-node-stack',
+//           createdAt: new Date(),
+//         },
+//         {
+//           id: genId(),
+//           slug: 'draft-post',
+//           title: 'Draft Post',
+//         },
+//       ],
+//     });
+//   }
+// };
+// seedDB();
 
 const app = express();
 const port = Number(process.env.PORT ?? 8080);
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.json ({hello: 'world'});
+app.get('/', async (req, res) => {
+  const posts = await db.post.findMany();
+  res.json(posts);
 });
 
 app.listen(port, '0.0.0.0', () => {
