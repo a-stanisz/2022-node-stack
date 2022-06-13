d0db00a64a78dae1a523f8a4daf0f4d4c7986d3d
a-stanisz <hello@adamstaniszewski.com>
2022-06-13 15:55:57 +0200

    Try to set up knex migration

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import { PrismaClient } from '@prisma/client';
-// import { nanoid } from 'nanoid/async';
-
 // import { randomUUID } from 'crypto';
 import express from 'express';
 import morgan from 'morgan';
@@ -8,7 +6,7 @@ import morgan from 'morgan';
 const db = new PrismaClient({ log: ['error', 'info', 'query', 'warn'] });
 
 // const seedDB = async () => {
-//   if ((await db.user.count()) === 0) {
+//   if (!(await db.user.count())) {
 //     await db.user.createMany({
 //       data: [
 //         {
@@ -23,17 +21,17 @@ const db = new PrismaClient({ log: ['error', 'info', 'query', 'warn'] });
 //       ],
 //     });
 //   }
-//   if ((await db.post.count()) === 0) {
+//   if (!(await db.post.count())) {
 //     await db.post.createMany({
 //       data: [
 //         {
-//           id: await nanoid(),
+//           id: randomUUID(),
 //           slug: 'node-stack',
 //           title: '2022-node-stack',
 //           createdAt: new Date(),
 //         },
 //         {
-//           id: await nanoid(),
+//           id: randomUUID(),
 //           slug: 'draft-post',
 //           title: 'Draft Post',
 //         },
@@ -47,9 +45,14 @@ const app = express();
 const port = Number(process.env.PORT ?? 8080);
 app.use(morgan('dev'));
 
-app.get('/', async (req, res) => {
+app.get('/posts', async (req, res) => {
   const posts = await db.post.findMany();
   res.json(posts);
+});
+
+app.get('/users', async (req, res) => {
+  const users = await db.user.findMany();
+  res.json(users);
 });
 
 app.listen(port, '0.0.0.0', () => {
